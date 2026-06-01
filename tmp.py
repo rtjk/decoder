c96e6e1a4db1f2b2b20b89213139095a9240537f
rtjk <47841774+rtjk@users.noreply.github.com>
2026-06-01 19:29:53 +0200

    -

--- tmp.py
+++ tmp.py
@@ -19,7 +19,7 @@ def compute_upcs(H_sparse, s, r, v):
 
 ################################################################################
 
-def fixed_counters_decrement(Htr_sparse, H_sparse, r, v):
+def compute_fixed_decr(Htr_sparse, H_sparse, r, v):
     fixed_decr = []
     for block in range(2):
         dense_s_block = vector(ZZ,r)
@@ -44,10 +44,10 @@ def update_upc(Htr_sparse, H_sparse, fixed_decr, s, r, v, pos, upc):
             shifted_decr[new_pos] = fixed_decr[block][i*r+j]
     upc += (-shifted_decr)
     # update upcs: +2
-    for i in range(v):
+    for i in range(v): # scan each idx of col corresponding to flipped pos
         row_index = (Htr_sparse[block][i] + shift)%r
-        if s[row_index] == 0:
-            for j in range(2):
+        if s[row_index] == 0: # if syndrome bit is 0
+            for j in range(2): # scan each index in the row
                 for k in range(v):
                     pos_toggle = j*r + (H_sparse[j][k] + row_index)%r
                     upc[pos_toggle] += 2
@@ -65,8 +65,8 @@ def bfmax_steroids(Htr_sparse, H_sparse, fixed_decr, s, r, v, num_iter_max):
         error[pos] += 1
         upc = update_upc(Htr_sparse, H_sparse, fixed_decr, s, r, v, pos, upc);
         # update syndrome
-        block = pos // r
-        shift = pos % r
+        block = pos//r
+        shift = pos%r
         for i in range(v):
             pos_toggle = (Htr_sparse[block][i]+shift)%r
             s[pos_toggle] += 1
@@ -76,6 +76,6 @@ def bfmax_steroids(Htr_sparse, H_sparse, fixed_decr, s, r, v, num_iter_max):
 
 ################################################################################
 
-fixed_decr = fixed_counters_decrement(Htr_sparse, H_sparse, r, v)
+fixed_decr = compute_fixed_decr(Htr_sparse, H_sparse, r, v)
 
 ################################################################################
